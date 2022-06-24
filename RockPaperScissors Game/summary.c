666a20a8d87cafe80edbf655b991e3cdaa81a595
soonyemma <69128107+soonyemma@users.noreply.github.com>
2022-06-24 13:18:47 +0100

    Create summary.c

---
+++ RockPaperScissors Game/summary.c
@@ -0,0 +1,8 @@
+// get computer choice
+char computerPick = getComputersChoice();
+ 
+// get user choice
+char userPick = getUserInput();
+ 
+// get result
+char result = getResult(userPick, computerPick);
