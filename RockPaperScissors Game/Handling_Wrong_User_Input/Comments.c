b13581cfbf073d04b4868e60c3120b9ffaba72bd
soonyemma <69128107+soonyemma@users.noreply.github.com>
2022-06-24 13:25:22 +0100

    Create Comments.c

---
+++ RockPaperScissors Game/Handling_Wrong_User_Input/Comments.c
@@ -0,0 +1,17 @@
+/*
+*get user choice
+*char userPick = getUserInput();
+*/
+
+Instead change to:
+
+/*
+// get input until user enters 'r', 'p' or 's'
+char userPick;
+while (1) {
+  userPick = getUserInput();
+  if (userPick == 'r' || userPick == 'p' || userPick == 's') {
+    break;
+  }
+}
+*/
